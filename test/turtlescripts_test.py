89b4c19d5ad00c8803744282ba36887eeced23f6
Seth Cook <seth.cook@purdue.edu>
2016-10-07 14:08:59 -0400

    asdf

---
+++ test/turtlescripts_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from TurtleScripts import TurtleScripts
+
+import unittest
+
+TEST_PROJECT_ID = 'gjdi6h'
+
+class TestTurtleScripts(unittest.TestCase):
+
+  def setUp(self):
+    self.ts = TurtleScripts()
+
+  def test_get_project(self):
+    print(self.ts.getProject(TEST_PROJECT_ID))
+    self.assertTrue(True)
+
+if( __name__ == '__main__' ):
+  unittest.main()
